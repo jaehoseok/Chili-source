ba19dd3903512eba7472fb8c855f19eb6b933c8b
최진호 <ho6778@naver.com>
2022-10-24 17:39:14 +0900

    ✨ feat(project service): 컨트롤러 기본 구성 추가

--- chilisource/microservices/project-service/src/main/java/com/ssafy/controller/ProjectController.java
+++ chilisource/microservices/project-service/src/main/java/com/ssafy/controller/ProjectController.java
@@ -1,7 +1,40 @@
 package com.ssafy.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.ssafy.config.loginuser.LoginUser;
+import com.ssafy.config.loginuser.User;
+import com.ssafy.entity.Project;
+import com.ssafy.service.ProjectService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@RequiredArgsConstructor
 @RestController
 public class ProjectController {
+    private final ProjectService projectService;
+
+    @GetMapping
+    public ResponseEntity<?> getProject(@LoginUser User user) {
+        List<Project> list = new ArrayList<>();
+        return new ResponseEntity<List<Project>>(list, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createProject() {
+        return new ResponseEntity<String>("", HttpStatus.OK);
+    }
+
+    @PutMapping
+    public ResponseEntity<?> updateProject() {
+        return new ResponseEntity<String>("", HttpStatus.OK);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<?> deleteProject() {
+        return new ResponseEntity<String>("", HttpStatus.OK);
+    }
 }
