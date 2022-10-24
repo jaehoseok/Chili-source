f7fe7afde215816c9437705c68cbe8b0b58095d8
박태이 <ehoi.loveyourself@gmail.com>
2022-10-24 17:33:10 +0900

    ✨ feat(issue-service): 이슈 템플릿 수정 api 구현 중

--- chilisource/microservices/issue-service/src/main/java/com/ssafy/controller/IssueTemplateController.java
+++ chilisource/microservices/issue-service/src/main/java/com/ssafy/controller/IssueTemplateController.java
@@ -1,5 +1,6 @@
 package com.ssafy.controller;
 
+import brave.Response;
 import com.ssafy.config.loginuser.LoginUser;
 import com.ssafy.config.loginuser.User;
 import com.ssafy.dto.IssueTemplateCreateRequest;
@@ -9,10 +10,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -41,7 +39,12 @@ public class IssueTemplateController {
         return ResponseEntity.status(HttpStatus.OK)
                 .build();
     }
+
     // 이슈 템플릿 수정
+//    @PutMapping("{issueTemplateId}")
+//    public ResponseEntity<?> updateIssueTemplate(@PathVariable Long issueTemplateId) {
+//
+//    }
     // 이슈 템플릿 삭제
     // 미들 버킷 리스트 조회
     // 미들 버킷 조회
