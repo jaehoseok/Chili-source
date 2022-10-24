216afaf29bd51f7ad42047347e2fccfdb6a081de
최진호 <ho6778@naver.com>
2022-10-24 17:38:16 +0900

    ✨ feat(project service): project 리스트 조회 추가

--- chilisource/microservices/project-service/src/main/java/com/ssafy/repository/ProjectRepo.java
+++ chilisource/microservices/project-service/src/main/java/com/ssafy/repository/ProjectRepo.java
@@ -2,8 +2,6 @@ package com.ssafy.repository;
 
 import com.ssafy.entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ProjectRepo extends JpaRepository<Project, Long>, ProjectCustomRepo {
 }
