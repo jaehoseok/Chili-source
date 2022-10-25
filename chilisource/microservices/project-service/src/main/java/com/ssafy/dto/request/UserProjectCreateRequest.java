bd0e0eff5c5e37f7dc1236d5ca0f1b3e26772196
최진호 <ho6778@naver.com>
2022-10-25 17:41:19 +0900

    feat(project team): project 팀원 CRUD dto 구현

---
+++ chilisource/microservices/project-service/src/main/java/com/ssafy/dto/request/UserProjectCreateRequest.java
@@ -0,0 +1,20 @@
+package com.ssafy.dto.request;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class UserProjectCreateRequest {
+    private String userColor = "FFFFFF";
+    private Long userId;
+    private Long projectId;
+
+    @Builder
+    public UserProjectCreateRequest( String userColor, Long userId, Long projectId) {
+        this.userColor = userColor;
+        this.userId = userId;
+        this.projectId = projectId;
+    }
+}
---
+++ chilisource/microservices/project-service/src/main/java/com/ssafy/dto/request/UserProjectUpdateRequest.java
@@ -0,0 +1,22 @@
+package com.ssafy.dto.request;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class UserProjectUpdateRequest {
+    private String userColor;
+    private Long userId;
+    private Long projectId;
+    private Long roleId;
+
+    @Builder
+    public UserProjectUpdateRequest(String userColor, Long userId, Long projectId, Long roleId) {
+        this.userColor = userColor;
+        this.userId = userId;
+        this.projectId = projectId;
+        this.roleId = roleId;
+    }
+}
