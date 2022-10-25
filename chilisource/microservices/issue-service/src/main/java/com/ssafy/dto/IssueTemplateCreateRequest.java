5c5bb8a5fb7f8bd81f12445bc7a4913f7bf3d938
박태이 <ehoi.loveyourself@gmail.com>
2022-10-25 14:38:58 +0900

    🐛 fix(issue-service): issue template 생성시 issue type id 대신 issue type name 으로 요청하게 수정

--- chilisource/microservices/issue-service/src/main/java/com/ssafy/dto/IssueTemplateCreateRequest.java
+++ chilisource/microservices/issue-service/src/main/java/com/ssafy/dto/IssueTemplateCreateRequest.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class IssueTemplateCreateRequest {
-    private Long issueTypeId;
+    private String issueType;
     private String summary;
     private String description;
     private String assignee;
@@ -16,17 +16,4 @@ public class IssueTemplateCreateRequest {
     private Long sprint;
     private Double storyPoints;
     private Long projectId;
-
-    @Builder
-    public IssueTemplateCreateRequest(Long issueTypeId, String summary, String description, String assignee, String priority, String epicLink, Long sprint, Double storyPoints, Long projectId) {
-        this.issueTypeId = issueTypeId;
-        this.summary = summary;
-        this.description = description;
-        this.assignee = assignee;
-        this.priority = priority;
-        this.epicLink = epicLink;
-        this.sprint = sprint;
-        this.storyPoints = storyPoints;
-        this.projectId = projectId;
-    }
 }
