3441fe2468663cd51cc909ef52728cb848f89769
beberiche <woaol@naver.com>
2022-10-25 23:30:52 +0900

    ♻️ refactor(컴포넌트 정리): select 컴포넌트 테스트 코드 삭제, 필요없는 import 삭제

--- front/src/components/pages/BucketPage.tsx
+++ front/src/components/pages/BucketPage.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const BucketPage = () => {
   return <div>BucketPage</div>;
 };
--- front/src/components/pages/WidgetSelectPage.tsx
+++ front/src/components/pages/WidgetSelectPage.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const WidgetSelectPage = () => {
   return <div>WidgetSelectPage</div>;
 };
