0dd32d31022ee92ab3a1708f1a6b11334336cd5b
최진호 <ho6778@naver.com>
2022-10-25 17:43:37 +0900

    ✨ feat(project team repo): 프로젝트 pk, 유저 pk 로 팀원 검색 레포지토리 추가

--- chilisource/microservices/project-service/src/main/java/com/ssafy/repository/UserProjectRepo.java
+++ chilisource/microservices/project-service/src/main/java/com/ssafy/repository/UserProjectRepo.java
@@ -4,6 +4,11 @@ import com.ssafy.entity.UserProject;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface UserProjectRepo extends JpaRepository<UserProject, Long>, UserProjectCustomRepo {
+    Optional<UserProject> findByUserIdAndProjectId(Long userId, Long projectId);
+    List<UserProject> findByProjectId(Long projectId);
 }
