63b033b2c07c7d721fec8a55077b1894cacd2c3f
DESKTOP-KVCQHCD\SSAFY <woaol@naver.com>
2022-10-25 14:36:21 +0900

    ♻️ refactor(랜딩페이지): text 컴포넌트 테스트용 코드 삭제

--- front/src/components/pages/LandingPage.tsx
+++ front/src/components/pages/LandingPage.tsx
@@ -1,7 +1,3 @@
-import Text from '../atoms/Text';
-
-const LandingPage = () => {
-  return <Text isFill={true} width={30} message={'에픽'} />;
-};
+const LandingPage = () => <div>LandingPage</div>;
 
 export default LandingPage;
