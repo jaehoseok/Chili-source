1f5da5b4ad7bebb671c4ffe8445db9c994a19bc4
beberiche <woaol@naver.com>
2022-10-27 00:17:08 +0900

    🐛 fix(tab 컴포넌트 style): 활성화, 비활성화 시 background-color 변경

--- front/src/components/atoms/Tab/style.ts
+++ front/src/components/atoms/Tab/style.ts
@@ -9,6 +9,6 @@ export const StyledTab = styled.span<StyledTypes>`
   ${tw`py-3 px-10 border rounded-t-2xl text-xl font-bold text-white `}
   display: inline-block;
   background-color: ${({ theme, activated }) =>
-    activated ? theme.color.primary : theme.color.secondary};
+    activated ? theme.color.secondary : theme.color.primary};
   cursor: ${({ activated }) => (activated ? 'none' : 'pointer')};
 `;
