96f6f4da1a26290c2c8430a64c5fabc90accc838
giftanchovy <24ph00@naver.com>
2022-10-27 16:44:17 +0900

    💎 style(button, circle): fE 컨벤션 중 import 시 묶음 규칙 적용

--- front/src/components/atoms/Button/style.ts
+++ front/src/components/atoms/Button/style.ts
@@ -1,4 +1,5 @@
 import React, { MouseEventHandler } from 'react';
+
 import styled, { css } from 'styled-components';
 
 export interface ButtonProps {
--- front/src/components/atoms/Circle/style.ts
+++ front/src/components/atoms/Circle/style.ts
@@ -1,4 +1,5 @@
 import React, { MouseEventHandler } from 'react';
+
 import styled, { css } from 'styled-components';
 import { theme } from '../../../styles/theme';
 
