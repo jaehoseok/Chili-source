7307e829afb6afa43af68470447667638e52c0b6
최진호 <ho6778@naver.com>
2022-10-27 10:52:41 +0900

    💎 style(project repositories): @Repository 삭제 통일

--- chilisource/microservices/project-service/src/main/java/com/ssafy/repository/ProjectTokenRepo.java
+++ chilisource/microservices/project-service/src/main/java/com/ssafy/repository/ProjectTokenRepo.java
@@ -2,8 +2,6 @@ package com.ssafy.repository;
 
 import com.ssafy.entity.ProjectGit;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ProjectTokenRepo extends JpaRepository<ProjectGit, Long>, ProjectTokenCustomRepo {
 }
--- chilisource/microservices/project-service/src/main/java/com/ssafy/repository/UserProjectRepo.java
+++ chilisource/microservices/project-service/src/main/java/com/ssafy/repository/UserProjectRepo.java
@@ -2,13 +2,12 @@ package com.ssafy.repository;
 
 import com.ssafy.entity.UserProject;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
-@Repository
 public interface UserProjectRepo extends JpaRepository<UserProject, Long>, UserProjectCustomRepo {
     Optional<UserProject> findByUserIdAndProjectId(Long userId, Long projectId);
+
     List<UserProject> findByProjectId(Long projectId);
 }
