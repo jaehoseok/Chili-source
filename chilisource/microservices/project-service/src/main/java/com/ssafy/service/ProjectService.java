bbc7395f17ebec8d34958345af7550992ff88425
최진호 <ho6778@naver.com>
2022-10-27 09:43:38 +0900

    💎 style(project service): 메서드간 빈줄 추가

--- chilisource/microservices/project-service/src/main/java/com/ssafy/service/ProjectService.java
+++ chilisource/microservices/project-service/src/main/java/com/ssafy/service/ProjectService.java
@@ -9,12 +9,16 @@ import java.util.List;
 public interface ProjectService {
     // 프로젝트 조회
     public ProjectResponse getProject(Long projectId);
+
     // 프로젝트 목록 조회
     public List<ProjectResponse> getProjectByUserId(Long userId);
+
     // 프로젝트 생성
     public void createProject(ProjectCreateRequest request, Long userId);
+
     // 프로젝트 내용 수정
     public void updateProject(ProjectUpdateRequest request);
+
     // 프로젝트 삭제
     public void deleteProject(Long projectId, Long userId);
 }
