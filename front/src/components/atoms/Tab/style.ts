2807f4def3e7178346031f2702628d8118eb75c1
DESKTOP-KVCQHCD\SSAFY <woaol@naver.com>
2022-10-27 13:39:20 +0900

    ♻️ refactor(tab 컴포넌트): tab 컴포넌트 사이즈 조정
    너무 크다! 좀더 작게 수정

--- front/src/components/atoms/Tab/style.ts
+++ front/src/components/atoms/Tab/style.ts
@@ -6,7 +6,7 @@ export interface styledType {
 }
 
 export const StyledTab = styled.span<styledType>`
-  ${tw`py-3 px-10 border rounded-t-2xl text-xl font-bold text-white `}
+  ${tw`py-2 px-8 border rounded-t-2xl text-lg font-bold text-white `}
   display: inline-block;
   background-color: ${({ theme, isActivated }) =>
     isActivated ? theme.color.secondary : theme.color.primary};
