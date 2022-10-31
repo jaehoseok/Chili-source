c5901692c97af8d6323b28b435e73f2ed977c4c1
DESKTOP-KVCQHCD\SSAFY <woaol@naver.com>
2022-10-31 14:25:43 +0900

    ♻️ refactor(project_dashboard_page): 필요없는 코드 제거

--- front/src/components/pages/ProjectDashBoardPage.tsx
+++ front/src/components/pages/ProjectDashBoardPage.tsx
@@ -1,18 +1,5 @@
-import InputBox from 'components/molecules/InputBox';
-
 const ProjectDashBoardPage = () => {
-  return (
-    <>
-      <InputBox
-        containerWidth="38rem"
-        containerPadding="20px"
-        inputWidth="30rem"
-        isRow={true}
-        labelName={'이름'}
-      ></InputBox>
-      {/* <div>ProjectDashBoardPage</div> */}
-    </>
-  );
+  return <div>ProjectDashBoardPage</div>;
 };
 
 export default ProjectDashBoardPage;
