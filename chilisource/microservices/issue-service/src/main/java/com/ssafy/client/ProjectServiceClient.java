7823f392388cb5cd031f436da0f32071e65f3b9c
박태이 <ehoi.loveyourself@gmail.com>
2022-11-01 15:46:47 +0900

    🐛 fix(issue-service): feign 주소 수정

--- chilisource/microservices/issue-service/src/main/java/com/ssafy/client/ProjectServiceClient.java
+++ chilisource/microservices/issue-service/src/main/java/com/ssafy/client/ProjectServiceClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "project-service", url = "http://localhost:8000/api/project-service")
+@FeignClient(name = "project-service", url = "https://k7b2071.p.ssafy.io:8000/project-service")
 public interface ProjectServiceClient {
     @GetMapping("/{id}")
     Response findProjectById(@PathVariable Long id);
