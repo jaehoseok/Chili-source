9a6380c1230cb8251d9306fb4749b8b9e9691c7d
박태이 <ehoi.loveyourself@gmail.com>
2022-11-01 17:25:05 +0900

    🐛 fix(issue-service): auth-service에서 feign 해오는 메서드 파라미터 수정

--- chilisource/microservices/issue-service/src/main/java/com/ssafy/client/AuthServiceClient.java
+++ chilisource/microservices/issue-service/src/main/java/com/ssafy/client/AuthServiceClient.java
@@ -1,16 +1,19 @@
 package com.ssafy.client;
 
+import com.ssafy.config.loginuser.LoginUser;
 import com.ssafy.config.loginuser.User;
 import com.ssafy.dto.response.TokenResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "auth-service", url = "https://k7b2071.p.ssafy.io:8000/auth-service")
 public interface AuthServiceClient {
     // TODO 사용자 아이디로 1. 사용자 이메일 2. 사용자 토큰 3. 사용자 지라 고유 아이디를 받아온다
     // TODO response 수정됐는지 확인하기
-    @GetMapping("/tokens")
-    List<TokenResponse> getToken(User user);
+    @GetMapping("/tokens/{tokenCodeId}")
+    TokenResponse getToken(
+            @LoginUser User user,
+            @PathVariable(name = "tokenCodeId") String tokenCodeId
+    );
 }
