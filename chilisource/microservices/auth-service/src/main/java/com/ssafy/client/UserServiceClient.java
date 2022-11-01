6d8a6932c5a91f6925d8d331458bfb45c50ec0f4
석재호 <wogh9705@naver.com>
2022-11-01 13:11:57 +0900

    Update chilisource/microservices/auth-service/src/main/java/com/ssafy/client/UserServiceClient.java, chilisource/microservices/issue-service/src/main/java/com/ssafy/client/ProjectServiceClient.java, chilisource/microservices/project-service/src/main/java/com/ssafy/client/AuthServiceClient.java, chilisource/microservices/widget-service/src/main/java/com/ssafy/client/AuthServiceClient.java, chilisource/microservices/widget-service/src/main/java/com/ssafy/client/ProjectServiceClient.java

--- chilisource/microservices/auth-service/src/main/java/com/ssafy/client/UserServiceClient.java
+++ chilisource/microservices/auth-service/src/main/java/com/ssafy/client/UserServiceClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "user-service", url = "http://k7b2071.p.ssafy.io/user-service")
+@FeignClient(name = "user-service", url = "https://k7b2071.p.ssafy.io/user-service")
 public interface UserServiceClient {
     @PostMapping("/users/{socialLoginType}")
     UserResponse findUser(
