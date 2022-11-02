0b970e30af41aafc17a344fae480be01ff378244
belle <ehoi.loveyourself@gmail.com>
2022-11-02 23:53:23 +0900

    refactor: 구현 코드 정리

--- chilisource/microservices/issue-service/src/main/java/com/ssafy/service/IssueServiceImpl.java
+++ chilisource/microservices/issue-service/src/main/java/com/ssafy/service/IssueServiceImpl.java
@@ -1,5 +1,6 @@
 package com.ssafy.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Charsets;
 import com.ssafy.client.AuthServiceClient;
 import com.ssafy.client.JiraFeignClient;
@@ -12,8 +13,8 @@ import com.ssafy.entity.IssueTemplate;
 import com.ssafy.entity.IssueType;
 import com.ssafy.entity.MiddleBucket;
 import com.ssafy.entity.MiddleBucketIssue;
+import com.ssafy.exception.BadRequestException;
 import com.ssafy.exception.DuplicateException;
-import com.ssafy.exception.InternalServerErrorException;
 import com.ssafy.exception.NotFoundException;
 import com.ssafy.exception.WrongFormException;
 import com.ssafy.repository.IssueTemplateRepo;
@@ -24,14 +25,11 @@ import feign.Response;
 import io.micrometer.core.instrument.util.IOUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.cloud.sleuth.http.HttpResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Base64;
 import java.util.Base64.Encoder;
@@ -300,6 +298,7 @@ public class IssueServiceImpl implements IssueService {
         MiddleBucketIssue middleBucketIssue = middleBucketIssueRepo.findById(middleBucketIssueId)
                 .orElseThrow(() -> new NotFoundException(MIDDLE_BUCKET_ISSUE_NOT_FOUND));
 
+        // TODO 리팩토링 하기
         // 예외처리 ver.1 : delete 실행 못함
 //        if (middleBucket.getMiddleBucketIssues().contains(middleBucketIssue)) {
 //            throw new NotFoundException(ISSUE_NOT_FOUND_IN_MIDDLE_BUCKET);
@@ -320,28 +319,26 @@ public class IssueServiceImpl implements IssueService {
         // 의문점 2. 왜 ver 2는 (2)를 선실행후 (1)을 나중에 실행하는지
     }
 
+    @Transactional
     @Override
-    public void addIssuesToJira(
-//            User user,
-            Long projectId, Long middleBucketId) {
+    public void addIssuesToJira(User user, Long projectId, Long middleBucketId) throws IOException {
         // 사용자 아이디로 1. 사용자 이메일 2. 사용자 토큰 3. 사용자 지라 고유 아이디를 받아온다
-//        TokenResponse response = authServiceClient.getToken(user, "jira");
+        TokenResponse response = authServiceClient.getToken(user, "jira");
 
         // 이메일과 토큰으로 Base64 인코딩을 한다
-//        String token = response.getEmail() + ":" + response.getValue();
-        // 테스트용
-        String token = "ehoi.loveyourself@gmail.com:DAgKZgAJGc8SZGDmwHf993C1";
+        String token = response.getEmail() + ":" + response.getValue();
+//        String token = "ehoi.loveyourself@gmail.com:DAgKZgAJGc8SZGDmwHf993C1"; // 테스트용
         String jiraToken = Base64.getEncoder().encodeToString(token.getBytes());
 
-        String userJiraTestId = "62beec7c268cac6e31c5e160"; // 임시
+        String userJiraTestId = "62beec7c268cac6e31c5e160"; // 테스트용
 //        String userJiraId = response.getSomething();
 
         // project-feign 으로 지라 프로젝트 코드를 가져온다.
-//        String jiraProjectCode = projectServiceClient.getProject(projectId)
-//                .getJiraProject();
+        String jiraProjectCode = projectServiceClient.getProject(projectId)
+                .getJiraProject();
         // 테스트용
-        String jiraProjectCode = "CHIL";
-        String jiraProjectId = "10000";
+//        String jiraProjectCode = "CHIL";
+//        String jiraProjectId = "10000";
 
         // 미들 버킷을 가져온다
         MiddleBucket middleBucket = middleBucketRepo.findById(middleBucketId)
@@ -356,15 +353,11 @@ public class IssueServiceImpl implements IssueService {
 
             // project : jira project code
             JiraIssueProjectCreateRequest project = JiraIssueProjectCreateRequest.builder()
-//                    .key(jiraProjectCode)
-                    .id(jiraProjectId)
+                    .key(jiraProjectCode)
+//                    .id(jiraProjectId)
                     .build();
 
-            System.out.println("프로젝트 키:" + project.getKey());
-
-
-            // 이슈 타입
-            // 에픽은 지라에서 직접 생성하고 여기서는 스토리, 태스크, 버그만 생성 가능
+            // 이슈 타입 : 에픽은 지라에서 직접 생성하고 여기서는 스토리, 태스크, 버그만 생성 가능
             String issueType = "";
             switch (issue.getIssueType().getName()) {
                 case "Story": {
@@ -387,7 +380,6 @@ public class IssueServiceImpl implements IssueService {
                     throw new IllegalArgumentException("알 수 없는 이슈 타입입니다.");
                 }
             }
-            System.out.println("이슈타입: " + issueType);
             JiraIssueTypeCreateRequest type = JiraIssueTypeCreateRequest.builder()
                     .id(issueType)
                     .build();
@@ -429,7 +421,7 @@ public class IssueServiceImpl implements IssueService {
 
             JiraIssueDetailCreateRequest fields = JiraIssueDetailCreateRequest.builder()
                     .summary(summary)
-                    .issueType(type)
+                    .issuetype(type)
                     .parent(parent)
                     .description(description)
                     .reporter(reporter)
@@ -444,23 +436,6 @@ public class IssueServiceImpl implements IssueService {
 
             issueUpdates.add(build);
         }
-        // 각 이슈들을 지라 이슈로 create 할 수 있게 dto 형식을 만든다
-        // 지라에 보낼 dto에 맞는 형식은..
-        // 1. summary : 이건 우리 이슈에 담아놓은 summary 그대로 가져다 쓴다
-        /*
-        2. parent 얘가 문제임 -> null 로 해결해보자
-        사용자가 만들려고 하는 이슈 타입이 에픽이면 parent는 필요없고 "customfield_100" 이라는 이름이 들어가야함
-        사용자가 만들려고 하는 이슈 타입이 스토리, 버그, 태스크면 parent에 해당 에픽의 ID를 가져와야함
-            -> 해당 어떻게 가져오지?
-         */
-        // 3. project는 id 이든 key 든 상관 없는데 이거는 project-service에서 feign으로 jira project code 가져오면 됨
-        // 4. description은 사용자가 입력한 정보로 처리하면 됨
-        // 5. reporter와 assignee는 내가 재호쪽으로 feign으로 요청해서 id 가져오면 됨
-        // + 이때 이메일이랑 토큰을 같이 가져오게
-        // 6. priority는 medium 이면 3으로 highest면 1로 이런 식으로 switch로 매핑...
-        // 7. storyPoints 는 db에 있는 값
-        // 8. 스프린트는 일단 바이이
-
         JiraIssueBulkCreateRequest bulk = JiraIssueBulkCreateRequest.builder()
                 .issueUpdates(issueUpdates)
                 .build();
@@ -472,13 +447,8 @@ public class IssueServiceImpl implements IssueService {
 //        String s = request.
 
         // 그걸 다시 list 형식으로 dto를 만든다 그걸 지라에 보낸다
-        Response response1 = jiraFeignClient.addIssuesToJira("Basic " + jiraToken, request);
+        Response response1 = jiraFeignClient.addIssuesToJira("Basic " + jiraToken, bulk);
         if (HttpStatus.Series.valueOf(response1.status()) != HttpStatus.Series.SUCCESSFUL) {
-            System.out.println("실패했어요!");
-            System.out.println(response1.status());
-            System.out.println(response1.reason());
-//            System.out.println(response1.body().);
-//            throw new InternalServerErrorException(response1.body().toString());
             String conflictionDetails;
             try {
                 conflictionDetails = IOUtils.toString(response1.body().asInputStream(), Charsets.UTF_8);
