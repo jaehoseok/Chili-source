691663caf0a8764820f40e0de93d9f36e2b6eff3
belle <ehoi.loveyourself@gmail.com>
2022-11-02 23:47:24 +0900

    🐛 fix(issue-service): jira api 에 맞게 uri 수정 (/bulk 추가)

--- chilisource/microservices/issue-service/src/main/java/com/ssafy/client/JiraFeignClient.java
+++ chilisource/microservices/issue-service/src/main/java/com/ssafy/client/JiraFeignClient.java
@@ -1,6 +1,6 @@
 package com.ssafy.client;
 
-import com.ssafy.dto.request.jira.JiraIssueFinalCreateRequest;
+import com.ssafy.dto.request.jira.JiraIssueBulkCreateRequest;
 import com.ssafy.dto.response.JiraEpicListResponse;
 import feign.Response;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -9,15 +9,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
+// TODO 개인 테스트 지라 프로젝트에서 수정하기
 @FeignClient(name = "jira", url = "https://ehoi-chili.atlassian.net/rest/api/3")
 public interface JiraFeignClient {
-    // TODO bulk 로 생성하는 api로 수정
-    // TODO 개인 테스트 지라 프로젝트에서 수정하기
-    // TODO 파라미터에 jira에서 원하는 형식으로 json 보내주기
-    @PostMapping("/issue")
+    // 지라에 이슈 추가
+    @PostMapping("/issue/bulk")
     Response addIssuesToJira(
             @RequestHeader("Authorization") String jiraToken,
-            @RequestBody JiraIssueFinalCreateRequest request
+            @RequestBody JiraIssueBulkCreateRequest request
     );
 
     // 해당 프로젝트에서 만든 에픽 리스트 조회
