7a6e7e5c5c934490eeea63339430f692aa96cd80
giftanchovy <24ph00@naver.com>
2022-11-02 17:53:18 +0900

    ✨ feat(issuespage): 컴포넌트 삽입
    집가서 작업 이어서 하려고 커밋

--- front/src/components/pages/IssuesPage.tsx
+++ front/src/components/pages/IssuesPage.tsx
@@ -1,5 +1,26 @@
+import { useRef, useState } from 'react';
+
+import IssueInfo from '../organisms/IssueInfo';
+import Issue from '../molecules/Issue';
+import NavProject from '../molecules/NavProject';
+import NavWidget from '../molecules/NavWidget';
+
 const IssuesPage = () => {
-  return <div>IssuesPage</div>;
+  // const [info, setInfo] = useState()
+  return (
+    <>
+      <Issue
+        summary={'이슈 제목'}
+        epicLink={'에픽'}
+        assignee={'담'}
+        rank={'L'}
+        type={'story'}
+        storyPoints={4}
+        clickHandler={() => console.log('click')}
+      />
+      <IssueInfo></IssueInfo>
+    </>
+  );
 };
 
 export default IssuesPage;
--- front/src/components/pages/LandingPage.tsx
+++ front/src/components/pages/LandingPage.tsx
@@ -1,22 +1,5 @@
-import IssueInfo from '../organisms/IssueInfo';
-import Issue from '../molecules/Issue';
-import NavProject from '../molecules/NavProject';
-import NavWidget from '../molecules/NavWidget';
-
 const LandingPage = () => {
-  return (
-    <>
-      <Issue
-        summary={'이슈 제목'}
-        epicLink={'에픽'}
-        assignee={'담'}
-        rank={'L'}
-        type={'story'}
-        storyPoints={4}
-      />
-      <IssueInfo></IssueInfo>
-    </>
-  );
+  return <div>LandingPage</div>;
 };
 
 export default LandingPage;
