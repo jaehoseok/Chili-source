33935de5f0e9f9c75a75574ef27b5e2c7b70ed9a
박태이 <ehoi.loveyourself@gmail.com>
2022-11-02 10:19:19 +0900

    ✨ feat(issue-service): 프로젝트 서비스에서 프로젝트 관련 정보를 받아오기 위한 feign 코드 작성

--- chilisource/microservices/issue-service/src/main/java/com/ssafy/client/ProjectServiceClient.java
+++ chilisource/microservices/issue-service/src/main/java/com/ssafy/client/ProjectServiceClient.java
@@ -1,12 +1,16 @@
 package com.ssafy.client;
 
+import com.ssafy.dto.response.ProjectResponse;
 import feign.Response;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "project-service", url = "https://k7b2071.p.ssafy/project-service")
+@FeignClient(name = "project-service", url = "https://k7b2071.p.ssafy.io:8000/project-service")
 public interface ProjectServiceClient {
     @GetMapping("/{id}")
     Response findProjectById(@PathVariable("id") Long id);
+
+    @GetMapping("/{projectId}")
+    ProjectResponse getProject(@PathVariable("projectId") Long projectId);
 }
