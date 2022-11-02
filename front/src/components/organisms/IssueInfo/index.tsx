a72b9d96eaa1d1aed8c20bb2c26164d4208e8c6c
giftanchovy <24ph00@naver.com>
2022-11-02 16:51:56 +0900

    ✨ feat(issueinfo): uI 작업

---
+++ front/src/components/organisms/IssueInfo/index.tsx
@@ -0,0 +1,48 @@
+import { StyledIssueInfo } from './style';
+import Sheet from '../../atoms/Sheet';
+import Text from '../../atoms/Text';
+import SelectBox from '../../molecules/SelectBox';
+import TextAreaBox from '../../molecules/TextAreaBox';
+
+const index = () => {
+  return (
+    <StyledIssueInfo>
+      <Sheet isShadow={false} flex={'column'}>
+        <TextAreaBox isRow={false} labelName={'프로젝트'} />
+        <SelectBox labelName={'이슈 유형'}>
+          <option value="">유형을 선택하세요</option>
+          <option value="story">스토리</option>
+          <option value="task">태스크</option>
+          <option value="bug">버그</option>
+        </SelectBox>
+        <TextAreaBox isRow={false} labelName={'요약'} />
+        <TextAreaBox isRow={false} labelName={'설명'} />
+        <SelectBox labelName={'보고자'}>
+          <option>팀원</option>
+          <option>팀원2</option>
+        </SelectBox>
+        <SelectBox labelName={'담당자'}>
+          <option>팀원</option>
+          <option>팀원2</option>
+        </SelectBox>
+        <span style={{ color: '#4BADE8', cursor: 'pointer' }}>나에게 할당</span>
+        <SelectBox labelName={'우선순위'}>
+          <option>Highest</option>
+          <option>High</option>
+          <option>Medium</option>
+          <option>Low</option>
+          <option>Lowest</option>
+        </SelectBox>
+        <SelectBox labelName={'Epic Link'}>
+          <option>에픽 링크 목록</option>
+        </SelectBox>
+        <SelectBox labelName={'Sprint'}>
+          <option>현재 스프린트(클릭 불가능하게 할거임)</option>
+        </SelectBox>
+        <TextAreaBox isRow={false} labelName={'Story Points'} />
+      </Sheet>
+    </StyledIssueInfo>
+  );
+};
+
+export default index;
--- front/src/components/pages/LandingPage.tsx
+++ front/src/components/pages/LandingPage.tsx
@@ -1,5 +1,22 @@
+import IssueInfo from '../organisms/IssueInfo';
+import Issue from '../molecules/Issue';
+import NavProject from '../molecules/NavProject';
+import NavWidget from '../molecules/NavWidget';
+
 const LandingPage = () => {
-  return <div>LandingPage</div>;
+  return (
+    <>
+      <Issue
+        summary={'이슈 제목'}
+        epicLink={'에픽'}
+        assignee={'담'}
+        rank={'L'}
+        type={'story'}
+        storyPoints={4}
+      />
+      <IssueInfo></IssueInfo>
+    </>
+  );
 };
 
 export default LandingPage;
