852b5ee47121158c3c13981159f478d90d769dfc
박태이 <ehoi.loveyourself@gmail.com>
2022-11-02 13:11:03 +0900

    🐛 fix(issue-service): @PathVariable value 지정

--- chilisource/microservices/issue-service/src/main/java/com/ssafy/client/ProjectServiceClient.java
+++ chilisource/microservices/issue-service/src/main/java/com/ssafy/client/ProjectServiceClient.java
@@ -8,5 +8,5 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "project-service", url = "https://k7b207.p.ssafy.io/project-service")
 public interface ProjectServiceClient {
     @GetMapping("/{id}")
-    Response findProjectById(@PathVariable Long id);
+    Response findProjectById(@PathVariable("id") Long id);
 }
