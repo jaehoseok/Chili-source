7fd242267cbd18a4819a9fbaf32426f34841d305
최진호 <ho6778@naver.com>
2022-11-03 16:48:00 +0900

    ✨ feat(gantt-service): 간트차트 최신버전 검색, 버전별 검색 API 구현

--- chilisource/microservices/project-service/src/main/java/com/ssafy/controller/GanttController.java
+++ chilisource/microservices/project-service/src/main/java/com/ssafy/controller/GanttController.java
@@ -23,16 +23,32 @@ public class GanttController {
     private final GanttChartService ganttChartService;
 
     @GetMapping
-    @ApiOperation(value = "간트 차트 조회")
+    @ApiOperation(value = "최신 버전 간트 차트 조회")
     public ResponseEntity<List<GanttChartResponse>> getGanttChart(
             @ApiParam(value = "검색 옵션 1(모든 팀원 조회) / 2(개별 차트 조회)") @RequestParam Integer op,
             @ApiParam(value = "프로젝트 pk") @RequestParam Long projectId,
             @ApiParam(value = "검색할 유저 pk (null 일 경우 공통 차트)") @RequestParam Long userId) {
         List<GanttChartResponse> responses;
         if (op.equals(2)) {
-            responses = ganttChartService.getProjectFilteredGanttChart(userId, projectId);
+            responses = ganttChartService.getProjectGanttChartEachLatest(userId, projectId);
         } else {
-            responses = ganttChartService.getProjectGanttChart(projectId);
+            responses = ganttChartService.getProjectGanttChartAllLatest(projectId);
+        }
+        return ResponseEntity.ok(responses);
+    }
+
+    @GetMapping("/version")
+    @ApiOperation(value = "특정 버전 간트 차트 조회")
+    public ResponseEntity<List<GanttChartResponse>> getGanttChartByVersion(
+            @ApiParam(value = "검색 옵션 1(모든 팀원 조회) / 2(개별 차트 조회)") @RequestParam Integer op,
+            @ApiParam(value = "프로젝트 pk") @RequestParam Long projectId,
+            @ApiParam(value = "검색할 유저 pk (null 일 경우 공통 차트)") @RequestParam Long userId,
+            @ApiParam(value = "검색할 간트차트 버전") @RequestParam Long version) {
+        List<GanttChartResponse> responses;
+        if (op.equals(2)) {
+            responses = ganttChartService.getProjectGanttChartByVersion(projectId, version);
+        } else {
+            responses = ganttChartService.getProjectGanttChartByVersionEach(userId, projectId, version);
         }
         return ResponseEntity.ok(responses);
     }
