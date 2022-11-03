a04896f85f71cb142be7d8176bbd6499c4125f6c
giftanchovy <24ph00@naver.com>
2022-11-03 14:09:21 +0900

    ✨ feat(issuepage): 이슈 템플릿 데이터 이슈 정보 컴포넌트에 매핑 구현

--- front/src/components/organisms/IssueInfo/index.tsx
+++ front/src/components/organisms/IssueInfo/index.tsx
@@ -4,18 +4,19 @@ import Text from '../../atoms/Text';
 import SelectBox from '../../molecules/SelectBox';
 import TextAreaBox from '../../molecules/TextAreaBox';
 
-const index = () => {
+const index = (props: any) => {
+  console.log(props);
   return (
     <StyledIssueInfo>
       <Sheet isShadow={false} flex={'column'}>
-        <TextAreaBox isRow={false} labelName={'프로젝트'} />
+        <TextAreaBox isRow={false} labelName={'프로젝트'} textAreaValue={props.info.project} />
         <SelectBox labelName={'이슈 유형'}>
           <option value="">유형을 선택하세요</option>
           <option value="story">스토리</option>
           <option value="task">태스크</option>
           <option value="bug">버그</option>
         </SelectBox>
-        <TextAreaBox isRow={false} labelName={'요약'} />
+        <TextAreaBox isRow={false} labelName={'요약'} textAreaValue={props.info.summary} />
         <TextAreaBox isRow={false} labelName={'설명'} />
         <SelectBox labelName={'보고자'}>
           <option>팀원</option>
@@ -39,7 +40,11 @@ const index = () => {
         <SelectBox labelName={'Sprint'}>
           <option>현재 스프린트(클릭 불가능하게 할거임)</option>
         </SelectBox>
-        <TextAreaBox isRow={false} labelName={'Story Points'} />
+        <TextAreaBox
+          isRow={false}
+          labelName={'Story Points'}
+          textAreaValue={props.info.storyPoints}
+        />
       </Sheet>
     </StyledIssueInfo>
   );
--- front/src/components/pages/IssuesPage.tsx
+++ front/src/components/pages/IssuesPage.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 
 import IssueInfo from '../organisms/IssueInfo';
 import Issue from '../molecules/Issue';
@@ -6,19 +6,43 @@ import NavProject from '../molecules/NavProject';
 import NavWidget from '../molecules/NavWidget';
 
 const IssuesPage = () => {
-  // const [info, setInfo] = useState()
+  const issue = {
+    project: '',
+    type: '',
+    summary: '',
+    reporter: '',
+    assignee: '',
+    rank: '',
+    epicLink: '',
+    sprint: '',
+    storyPoints: 0,
+  };
+  const [info, setInfo] = useState(issue);
+  const setInfoHandler = (props: any) => {
+    setInfo(props);
+  };
   return (
     <>
       <Issue
+        project={'프로젝트 명'}
         summary={'이슈 제목'}
         epicLink={'에픽'}
         assignee={'담'}
         rank={'L'}
         type={'story'}
-        storyPoints={4}
-        clickHandler={() => console.log('click')}
+        storyPoints={8}
+        clickHandler={setInfoHandler}
       />
-      <IssueInfo></IssueInfo>
+      <Issue
+        summary={'이슈 제목2'}
+        epicLink={'에픽2'}
+        assignee={'당'}
+        rank={'H'}
+        type={'task'}
+        storyPoints={4}
+        clickHandler={setInfoHandler}
+      />
+      <IssueInfo info={info}></IssueInfo>
     </>
   );
 };
