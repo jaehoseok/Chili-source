9b10f34da7f6cc1c2c00c56a6e737f3e823d0bf8
최진호 <ho6778@naver.com>
2022-11-04 23:41:28 +0900

    ✨ feat(project-gantt): 간트 차트 날짜 범위 검색 기능 추가

--- chilisource/microservices/project-service/src/main/java/com/ssafy/service/GanttChartService.java
+++ chilisource/microservices/project-service/src/main/java/com/ssafy/service/GanttChartService.java
@@ -4,20 +4,21 @@ import com.ssafy.dto.request.GanttChartCreateRequest;
 import com.ssafy.dto.request.GanttChartUpdateRequest;
 import com.ssafy.dto.response.GanttChartResponse;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface GanttChartService {
     // 프로젝트 내 최신 버전 전체 간트차트 조회
-    List<GanttChartResponse> getProjectGanttChartAllLatest(Long projectId);
+    List<GanttChartResponse> getProjectGanttChartAllLatest(Long projectId, LocalDateTime start, LocalDateTime end);
 
     // 프로젝트 내 최신 버전 개별/공통 간트차트 조회
-    List<GanttChartResponse> getProjectGanttChartEachLatest(Long userId, Long projectId);
+    List<GanttChartResponse> getProjectGanttChartEachLatest(Long userId, Long projectId, LocalDateTime start, LocalDateTime end);
 
     // 프로젝트 내 버전 별 전체 간트차트 조회
-    List<GanttChartResponse> getProjectGanttChartByVersion(Long projectId, Long version);
+    List<GanttChartResponse> getProjectGanttChartByVersion(Long projectId, Long version, LocalDateTime start, LocalDateTime end);
 
     // 프로젝트 내 버전 별 개별/공통 간트차트 조회
-    List<GanttChartResponse> getProjectGanttChartByVersionEach(Long userId, Long projectId, Long version);
+    List<GanttChartResponse> getProjectGanttChartByVersionEach(Long userId, Long projectId, Long version, LocalDateTime start, LocalDateTime end);
 
     // 간트차트 내용 추가
     void createGanttChart(Long userId, GanttChartCreateRequest request);
