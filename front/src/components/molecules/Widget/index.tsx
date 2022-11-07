67d396ea7466ef79c721a3e9c519fb4af4b1f747
JunHyuk Park <inte99ral@gmail.com>
2022-11-07 09:29:21 +0900

    ✨ feat(molecules): 위젯 용 모레큘

--- front/src/components/molecules/Widget/index.tsx
+++ front/src/components/molecules/Widget/index.tsx
@@ -1,20 +1,41 @@
+// API & Library
 import { ReactNode } from 'react';
+
+// styles
 import { StyledWidget, styledType } from './style';
 
+// components
+import Sheet from 'components/atoms/Sheet';
+
 interface propsType extends styledType {
+  type?: string;
   children?: ReactNode;
 }
 
 /**
  * @description
- * 만드는 중 입니다.
+ * 위젯을 만드는 컴포넌트
+ *
+ * @param {string?} type - #위젯 타입 문자열
  *
  * @author inte
  */
-export const Widget = ({}: propsType) => {
+export const Widget = ({ type, children, width, height }: propsType) => {
+  // 없는 위젯 타입 선언시, require 에러 핸들링
+
+  const widgetData = (type: string) => {
+    if (type == 'plus') return <>+</>;
+    return <>위젯내용</>;
+  };
+
   return (
     <>
-      <StyledWidget></StyledWidget>
+      <StyledWidget height={height} width={width}>
+        <Sheet isShadow={true} height="100%" width="100%">
+          <div>{children}</div>
+          <div>{widgetData(type || 'plus')}</div>
+        </Sheet>
+      </StyledWidget>
     </>
   );
 };
