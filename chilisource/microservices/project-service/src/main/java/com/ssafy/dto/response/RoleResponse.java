70a73672e26d820590da6c8906e4b45409df2b2e
최진호 <ho6778@naver.com>
2022-11-08 13:40:48 +0900

    ✨ feat(project-service): swagger 설명 추가

--- chilisource/microservices/project-service/src/main/java/com/ssafy/dto/response/RoleResponse.java
+++ chilisource/microservices/project-service/src/main/java/com/ssafy/dto/response/RoleResponse.java
@@ -1,6 +1,7 @@
 package com.ssafy.dto.response;
 
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,14 +10,19 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @ApiModel(value = "권한 정보")
 public class RoleResponse {
+    @ApiModelProperty(value = "권한 pk")
     private String id;
 
+    @ApiModelProperty(value = "수정 권한")
     private Boolean modify;
 
+    @ApiModelProperty(value = "초대 권한")
     private Boolean invite;
 
+    @ApiModelProperty(value = "강퇴 권한")
     private Boolean fire;
 
+    @ApiModelProperty(value = "삭제 권한")
     private Boolean remove;
 
     @Builder
