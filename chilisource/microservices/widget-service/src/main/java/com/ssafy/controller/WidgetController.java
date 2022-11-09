acb76aeedbfa0cd8a4f1d261ba3cd37b57aadf12
jaehoseok <wogh9705@naver.com>
2022-11-09 14:30:14 +0900

    🐛 fix(widget-service): feign error fix

--- chilisource/microservices/widget-service/src/main/java/com/ssafy/controller/WidgetController.java
+++ chilisource/microservices/widget-service/src/main/java/com/ssafy/controller/WidgetController.java
@@ -143,9 +143,8 @@ public class WidgetController {
         String accessToken = request.getHeader(HttpHeaders.AUTHORIZATION);
         switch (type) {
             case SSAFYGITLAB: {
-                System.out.println("branch : " + branch + " is Empty : " + branch.isEmpty() );
                 if (branch.isEmpty())
-                    return ResponseEntity.ok(ssafyGitlabService.findMergeRequest(accessToken, type.name(), projectId, user.getId()));
+                    return ResponseEntity.ok(ssafyGitlabService.findMergeRequest(accessToken, tokenCodeId, projectId, user.getId()));
                 else
                     return ResponseEntity.ok(ssafyGitlabService.findCommits(accessToken, tokenCodeId, projectId, user.getId(), branch));
             }
