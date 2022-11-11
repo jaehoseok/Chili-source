551d3eb639dbdd4a2d5bf44aaf5d884bfb3debff
beberiche <woaol@naver.com>
2022-11-11 23:20:39 +0900

    ✨ feat(projectsettingpage 폴더): 프로젝트 셋팅 페이지 폴더 생성, HeaderServiceNav 렌더링, Header 이벤트 정상작동

---
+++ front/src/components/pages/ProjectSettingPage/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import HeaderServiceNav from 'components/organisms/common/HeaderServiceNav';
+
+const index = () => {
+  return (
+    <>
+      <HeaderServiceNav></HeaderServiceNav>
+      <div>ProjectSettingPage</div>
+    </>
+  );
+};
+
+export default index;
