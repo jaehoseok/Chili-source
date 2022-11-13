63f7717ce1fa06eab9f76e183701d7def35d2f19
beberiche <woaol@naver.com>
2022-11-13 20:31:43 +0900

    ✨ feat(지라 이슈 가져오기): 지라 이슈 가져오기 api 연결 완료

--- front/src/api/rest/issue/index.ts
+++ front/src/api/rest/issue/index.ts
@@ -54,4 +54,75 @@ export default {
       console.log(e);
     }
   },
+
+  /**
+   * @description
+   * 지라 이슈 가운데 아직 done 하지 않은 이슈 모두 가져오기
+   */
+
+  getIssuesNotDone: async (projectId: number) => {
+    interface responseType {
+      fields: {
+        assignee: {
+          accountId: string;
+          displayName: string;
+          emailAddress: string;
+        };
+        customfield_10020: [
+          {
+            goal: string;
+            id: number;
+            name: string;
+            state: string;
+          },
+        ];
+        customfield_10031: number;
+        issuetype: {
+          id: string;
+          name: string;
+        };
+        parent: {
+          fields: {
+            summary: string;
+          };
+          id: string;
+          key: string;
+        };
+        priority: {
+          id: string;
+          name: string;
+        };
+        project: {
+          id: string;
+          key: string;
+          name: string;
+        };
+        reporter: {
+          accountId: string;
+          displayName: string;
+          emailAddress: string;
+        };
+        status: {
+          id: string;
+          name: string;
+        };
+        summary: {
+          summary: string;
+        };
+      };
+      id: string;
+      key: string;
+    }
+
+    return new Promise<responseType[]>((resolve, reject) => {
+      issueAxios
+        .get(`/jira/issues/todo/${projectId}`)
+        .then(response => {
+          resolve(response.data.issues);
+        })
+        .catch(error => {
+          reject(error);
+        });
+    });
+  },
 };
--- front/src/components/organisms/project/calendar/Sidebar/index.tsx
+++ front/src/components/organisms/project/calendar/Sidebar/index.tsx
@@ -1,9 +1,15 @@
-import { useEffect, useState, memo } from 'react';
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+
+import { useGetIssuesNotDone } from 'hooks/issue';
+import { useGetUserInfoHandler } from 'hooks/user';
+import { useGetTeamForProject } from 'hooks/project';
 
 import { StyledJiraIssues } from './style';
-import { Draggable } from '@fullcalendar/interaction';
 
 import Issue from 'components/molecules/Issue';
+
+import { Draggable } from '@fullcalendar/interaction';
 
 /**
  * @description
@@ -12,59 +18,80 @@ import Issue from 'components/molecules/Issue';
  *
  * @author bell
  */
-const index = memo(() => {
-  const [externalEvents, setExternalEvents] = useState([
-    {
-      title: '구글 로그인 구현',
-      type: 'story',
-      reporter: '박준혁',
-      assignee: '박준혁',
-      epicLink: '개발',
-      storyPoints: 2,
-      color: '#63BA3C',
-      id: 34432,
-    },
-    {
-      title: '티 타임',
-      type: 'task',
-      reporter: '박성현',
-      assignee: '박성현',
-      epicLink: '기타',
-      storyPoints: 4,
-      color: '#4BADE8',
-      id: 323232,
-    },
-    {
-      title: '네비게이션 바 제작',
-      type: 'story',
-      reporter: '석재호',
-      assignee: '석재호',
-      epicLink: '개발',
-      storyPoints: 4,
-      color: '#63BA3C',
-      id: 1111,
-    },
-    {
-      title: '미들 버킷',
-      type: 'bug',
-      reporter: '박태이',
-      assignee: '박태이',
-      epicLink: '개발',
-      storyPoints: 2,
-      color: '#E5493A',
-      id: 432432,
-    },
-    {
-      title: '간트 차트',
-      type: 'story',
-      reporter: '최진호',
-      assignee: '최진호',
-      epicLink: '개발',
-      storyPoints: 1,
-      color: '#63BA3C',
-      id: 432123,
-    },
-  ]);
+const index = () => {
+  const location = useLocation();
+  const projectId = +location.pathname.split('/')[2];
+
+  const getIssuesNotDone = useGetIssuesNotDone(projectId);
+  const getUserInfo = useGetUserInfoHandler();
+  const getTeamForProject = useGetTeamForProject(projectId);
+
+  const myInfo = () => {
+    if (getTeamForProject.data && getUserInfo.data) {
+      const idx = getTeamForProject.data.findIndex(item => item.userId === getUserInfo.data.id);
+      if (idx > -1) {
+        return getTeamForProject.data[idx];
+      }
+    }
+  };
+
+  const currentColor = myInfo()?.userColor;
+  const currentImage = myInfo()?.userImage;
+
+  console.log(getIssuesNotDone.data);
+
+  // const [externalEvents, setExternalEvents] = useState([
+  //   {
+  //     title: '구글 로그인 구현',
+  //     type: 'story',
+  //     reporter: '박준혁',
+  //     assignee: '박준혁',
+  //     epicLink: '개발',
+  //     storyPoints: 2,
+  //     color: '#63BA3C',
+  //     id: 34432,
+  //   },
+  //   {
+  //     title: '티 타임',
+  //     type: 'task',
+  //     reporter: '박성현',
+  //     assignee: '박성현',
+  //     epicLink: '기타',
+  //     storyPoints: 4,
+  //     color: '#4BADE8',
+  //     id: 323232,
+  //   },
+  //   {
+  //     title: '네비게이션 바 제작',
+  //     type: 'story',
+  //     reporter: '석재호',
+  //     assignee: '석재호',
+  //     epicLink: '개발',
+  //     storyPoints: 4,
+  //     color: '#63BA3C',
+  //     id: 1111,
+  //   },
+  //   {
+  //     title: '미들 버킷',
+  //     type: 'bug',
+  //     reporter: '박태이',
+  //     assignee: '박태이',
+  //     epicLink: '개발',
+  //     storyPoints: 2,
+  //     color: '#E5493A',
+  //     id: 432432,
+  //   },
+  //   {
+  //     title: '간트 차트',
+  //     type: 'story',
+  //     reporter: '최진호',
+  //     assignee: '최진호',
+  //     epicLink: '개발',
+  //     storyPoints: 1,
+  //     color: '#63BA3C',
+  //     id: 432123,
+  //   },
+  // ]);
 
   useEffect(() => {
     const draggableEl = document.getElementById('external-events');
@@ -90,35 +117,36 @@ const index = memo(() => {
   return (
     <StyledJiraIssues>
       <div id="external-events">
-        {externalEvents.map(
-          ({ title, id, color, type, storyPoints, reporter, assignee, epicLink }) => (
+        {getIssuesNotDone.data &&
+          getIssuesNotDone.data.map(({ id, fields, key }) => (
             <div
               className="fc-event fc-h-event mb-1 fc-daygrid-event fc-daygrid-block-event p-2"
-              title={title}
+              title={fields.summary.summary}
               data-id={id}
-              data-color={color}
-              key={id}
+              data-color={currentColor}
+              key={key}
             >
-              {/* <div className="fc-event-main">
-              <div>
-                <strong>{event.title}</strong>
-              </div>
-            </div> */}
               <Issue
-                templateId={id}
-                summary={title}
-                type={type}
-                assignee={assignee}
-                reporter={reporter}
-                storyPoints={storyPoints}
-                epicLink={epicLink}
+                userImage={currentImage as string}
+                templateId={+id}
+                summary={fields.summary.summary}
+                type={
+                  fields.issuetype.name == '스토리'
+                    ? 'story'
+                    : fields.issuetype.name == '작업'
+                    ? 'task'
+                    : 'bug'
+                }
+                assignee={fields.assignee.displayName}
+                reporter={fields.assignee.displayName}
+                storyPoints={fields.customfield_10031}
+                epicLink={fields.parent.fields.summary}
               ></Issue>
             </div>
-          ),
-        )}
+          ))}
       </div>
     </StyledJiraIssues>
   );
-});
+};
 
 export default index;
