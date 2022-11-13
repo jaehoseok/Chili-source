186633911121ceff0850423a1512bd8d70d551c8
jaehoseok <wogh9705@naver.com>
2022-11-13 10:58:29 +0900

    ✨ feat(user-service): search user

--- chilisource/microservices/user-service/src/main/java/com/ssafy/controller/UserController.java
+++ chilisource/microservices/user-service/src/main/java/com/ssafy/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.ssafy.controller;
 
+import brave.Response;
 import com.ssafy.config.AwsS3Service;
 import com.ssafy.config.Constant;
 import com.ssafy.config.loginuser.LoginUser;
@@ -78,5 +79,13 @@ public class UserController {
             @ApiParam(value = "유저 pk 리스트") @RequestParam List<Long> userIds
     ) {
         return ResponseEntity.ok(userService.getUserList(userIds));
+    }
+
+    @GetMapping("/users/search")
+    @ApiOperation(value = "유저 검색")
+    public ResponseEntity<?> searchUser(
+            @ApiParam(value = "유저 email") @RequestParam String email
+    ) {
+        return ResponseEntity.ok(userService.getUserList(email));
     }
 }
